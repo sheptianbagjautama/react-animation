f4b1127a23a2241dd8b5dfc9961b4cc9443bd7f2
Sheptian Bagja Utama <sheptianbagjautama@gmail.com>
2024-08-21 10:27:27 +0700

    Reusing animation states

--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -2,14 +2,23 @@ import { motion } from "framer-motion";
 import { createPortal } from "react-dom";
 
 export default function Modal({ title, children, onClose }) {
+  // const hiddenAnimationState = { opacity: 0, y: 30 };
   return createPortal(
     <>
       <div className="backdrop" onClick={onClose} />
       <motion.dialog
-        // initial itu state awal animasi ketika component dialog ini muncul , karena tidak ada trigger props untuk memunculkan component dialog ini, jadi menggunakan props initial
-        initial={{ opacity: 0, y: 30 }}
-        animate={{ opacity: 1, y: 0 }} //lakukan animasi setelah initial
-        exit={{ opacity: 0, y: 30 }} //animasi ketika ingin menghilangkan component
+        // // initial itu state awal animasi ketika component dialog ini muncul , karena tidak ada trigger props untuk memunculkan component dialog ini, jadi menggunakan props initial
+        // initial={hiddenAnimationState}
+        // animate={{ opacity: 1, y: 0 }} //lakukan animasi setelah initial
+        // exit={hiddenAnimationState} //animasi ketika ingin menghilangkan component
+
+        variants={{
+          hidden: { opacity: 0, y: 30 },
+          visible: { opacity: 1, y: 0 },
+        }}
+        initial="hidden"
+        animate="visible"
+        exit="hidden"
         open
         className="modal"
       >
