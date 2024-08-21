c9f87ff6ae9176cd4571fffacecc06ab227045c2
Sheptian Bagja Utama <sheptianbagjautama@gmail.com>
2024-08-21 10:22:06 +0700

    Making elements "Pop" with hover animations

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,8 +1,8 @@
 import { useState } from "react";
 
 import NewChallenge from "./NewChallenge.jsx";
-// Wrapper untuk display atau remove component terutama untuk component yang ingin di remove contoh seperti ketika hide modal
-import { AnimatePresence } from "framer-motion";
+// AnimatePresence = Wrapper untuk display atau remove component terutama untuk component yang ingin di remove contoh seperti ketika hide modal
+import { AnimatePresence, motion } from "framer-motion";
 
 export default function Header() {
   const [isCreatingNewChallenge, setIsCreatingNewChallenge] = useState();
@@ -23,9 +23,14 @@ export default function Header() {
 
       <header id="main-header">
         <h1>Your Challenges</h1>
-        <button onClick={handleStartAddNewChallenge} className="button">
+        <motion.button
+          whileHover={{ scale: 1.1 }}
+          transition={{ type: "spring", stiffness: 500 }}
+          onClick={handleStartAddNewChallenge}
+          className="button"
+        >
           Add Challenge
-        </button>
+        </motion.button>
       </header>
     </>
   );
